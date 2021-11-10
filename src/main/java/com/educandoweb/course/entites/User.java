715380c93436fb00518f7b69231b0913f9e23648
josemar666 <josemarlob1@hotmail.com>
2021-11-09 21:04:01 -0300

    JPA repository, dependency injection, database seeding

--- src/main/java/com/educandoweb/course/entites/User.java
+++ src/main/java/com/educandoweb/course/entites/User.java
@@ -25,10 +25,21 @@ public class User implements Serializable {
 	public User() {
 		
 	}
+	
+	public User ( String name, String email, String phone, String password) {
+		
+		
+		this.name = name;
+		this.email = email;
+		this.phone = phone;
+		this.password = password;
+		
+	}
+	
 
-	public User(long i, String name, String email, String phone, String password) {
+	public User(long id, String name, String email, String phone, String password) {
 		super();
-		this.id = i;
+		this.id = id;
 		this.name = name;
 		this.email = email;
 		this.phone = phone;
