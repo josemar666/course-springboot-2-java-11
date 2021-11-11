df4ae81d839806699846e4845408d2181010877d
josemar666 <josemarlob1@hotmail.com>
2021-11-11 20:03:59 -0300

    OrderStatus enum

--- src/main/java/com/educandoweb/course/entites/Order.java
+++ src/main/java/com/educandoweb/course/entites/Order.java
@@ -12,6 +12,7 @@ import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
+import com.educandoweb.course.entites.enums.OrderStatus;
 import com.fasterxml.jackson.annotation.JsonFormat;
 
 
@@ -31,15 +32,18 @@ public class Order implements Serializable {
   @JoinColumn(name = "client_id")
   private User client;
   
+  private Integer orderStatus;
+  
   public Order() {
 	  
   }
   
 
-public Order(Long id, Instant moment, User client) {
+public Order(Long id, Instant moment,OrderStatus orderStatus, User client) {
 	super();
 	this.id = id;
 	this.moment = moment;
+	setOrderStatus(orderStatus);
 	this.client = client;
 }
 
@@ -67,6 +71,20 @@ public User getClient() {
 public void setClient(User client) {
 	this.client = client;
 }
+
+
+
+public OrderStatus getOrderStatus() {
+	return OrderStatus.ValueOf( orderStatus);
+}
+
+
+public void setOrderStatus(OrderStatus orderStatus) {
+	if(orderStatus != null ) {
+	this.orderStatus = orderStatus.getCode();
+	}
+}
+
 
 @Override
 public int hashCode() {
