2b8774b50d4a5cdfb097678c2d0f06c4e6a6a3b1
josemar666 <josemarlob1@hotmail.com>
2021-11-18 19:41:27 -0300

    User update

--- src/main/java/com/educandoweb/course/resources/UserResources.java
+++ src/main/java/com/educandoweb/course/resources/UserResources.java
@@ -3,19 +3,17 @@ package com.educandoweb.course.resources;
 import java.net.URI;
 import java.util.List;
 
-import javax.servlet.Servlet;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
-import org.springframework.web.util.UriComponents;
 
 import com.educandoweb.course.entites.User;
 import com.educandoweb.course.services.UserService;
@@ -52,6 +50,11 @@ public class UserResources {
 		 service.delete(id);
 		 return ResponseEntity.noContent().build();
 		 
+	 }
+	 @PutMapping(value = "/{id}")
+	 public ResponseEntity<User> update(@PathVariable Long id ,@RequestBody User obj ){
+		 obj = service.update(id, obj);
+		 return ResponseEntity.ok().body(obj);
 	 }
 	
 }
