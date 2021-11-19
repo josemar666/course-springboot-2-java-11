e0b33fecfb42d7961be73654287fcde2433a5080
josemar666 <josemarlob1@hotmail.com>
2021-11-18 21:08:01 -0300

    Exception handling - delete

--- src/main/java/com/educandoweb/course/resources/exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/educandoweb/course/resources/exceptions/ResourceExceptionHandler.java
@@ -9,6 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import com.educandoweb.course.services.exceptions.DataBaseException;
 import com.educandoweb.course.services.exceptions.ResourceNotFoundException;
 
 @ControllerAdvice
@@ -22,5 +23,15 @@ public class ResourceExceptionHandler {
 	return ResponseEntity.status(status).body(err);
 		
 	}
+	
+	@ExceptionHandler(DataBaseException.class)
+	public ResponseEntity<StandarError> dataBase (DataBaseException e , HttpServletRequest request){
+	 String error = " Data Base Error";
+	 HttpStatus status = HttpStatus.BAD_REQUEST;
+	 StandarError err = new StandarError(Instant.now(),status.value(), error,e.getMessage(), request.getRequestURI());
+	return ResponseEntity.status(status).body(err);
+		
+	}
+	
 	
 }
